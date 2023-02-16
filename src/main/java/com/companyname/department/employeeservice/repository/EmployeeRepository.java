802f4be5685ddd18561d98cc93ca01bf3b6bc832
Parth Patel <parthpatel29feb@yahoo.com>
2023-02-16 00:00:52 -0500

    adding properties file

--- src/main/java/com/companyname/department/employeeservice/repository/EmployeeRepository.java
+++ src/main/java/com/companyname/department/employeeservice/repository/EmployeeRepository.java
@@ -4,5 +4,5 @@ import com.companyname.department.employeeservice.domain.DAOS.EmployeeDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeDAO, String> {
-//    EmployeeDAO findBy(String s);
+//    EmployeeDAO findByEmployeeID(String s);
 }
